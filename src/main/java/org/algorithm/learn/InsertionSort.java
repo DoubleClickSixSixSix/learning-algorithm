68e056766fa8db0fa301ebf589551e31abc79bae
yuhang.zhang <921007394@qq.com>
2017-10-29 22:54:23 +0800

    add insertion and selection sort

--- src/main/java/org/algorithm/learn/InsertionSort.java
+++ src/main/java/org/algorithm/learn/InsertionSort.java
@@ -1,9 +1,31 @@
 package org.algorithm.learn;
 
+import org.algorithm.learn.util.SwapData;
+
 /**
  * Created by yuhang.zhang on 2017/10/29.
  *
- * 插入排序
+ * 插入排序:
+ * 取出数组元素，比较，若小插入前面
+ * 与排好的各个元素比较并插入
  */
 public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,5,2,1,4};
+
+        for (int i = 1; i < arr.length; i++){
+            for (int j = i; j > 0; j--){
+                if (arr[j] < arr[j-1]){
+                    SwapData.swapIndex(arr,j,j-1);
+                }
+            }
+        }
+
+        for (int a : arr) {
+            System.out.print(a + "\t");
+        }
+    }
+
 }
--- src/main/java/org/algorithm/learn/SelectionSort.java
+++ src/main/java/org/algorithm/learn/SelectionSort.java
@@ -1,9 +1,36 @@
 package org.algorithm.learn;
 
+import org.algorithm.learn.util.SwapData;
+
 /**
  * Created by yuhang.zhang on 2017/10/29.
  *
  * 选择排序
  */
 public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,5,2,1,4};
+
+        for (int i = 0; i < arr.length-1; i++){
+            //取第i个元素为最小
+            int min = i;
+            for (int j = i+1; j < arr.length; j++){
+                if (arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            //如果min发生变化则交换
+            if (min != i){
+                SwapData.swapIndex(arr,min,i);
+            }
+        }
+
+        for (int a :
+             arr) {
+            System.out.print(a + "\t");
+        }
+
+    }
 }
